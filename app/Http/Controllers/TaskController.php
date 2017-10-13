e97903c031f323c383daed5c2db644c38a3db37d
SKILL573 <a9101021249@gmail.com>
2017-10-13 15:04:23 +0800

    驗證

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+   /**
+ * 建立新的任務。
+ *
+ * @param  Request  $request
+ * @return Response
+ */
+public function store(Request $request)
+{
+    $this->validate($request, [
+        'name' => 'required|max:255',
+    ]);
+
+    // Create The Task...
+}
+
 }
