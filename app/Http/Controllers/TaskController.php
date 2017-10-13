428076070a377f62b22747834527e1db58db4513
SKILL573 <a9101021249@gmail.com>
2017-10-13 15:27:56 +0800

    注入資源庫

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,10 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Task;
+use App\Http\Requests;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Repositories\TaskRepository;
+
 
 class TaskController extends Controller
 {
+    protected $tasks;
+    
    /**
  * 建立新的任務。
  *
@@ -20,6 +27,31 @@ public function store(Request $request)
 
     // Create The Task...
 }
+    /**
+     * 建立新的控制器實例。
+     *
+     * @param  TaskRepository  $tasks
+     * @return void
+     */
+public function __construct(TaskRepository $tasks)
+{
+    $this->middleware('auth');
+
+    $this->tasks = $tasks;
+}
+    /**
+     * 取得給定使用者的所有任務。
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+     public function index(Request $request)
+     {
+         return view('tasks.index', [
+             'tasks' => $this->tasks->forUser($request->user()),
+         ]);
+     } 
+
 /**
  * Display a list of all of the user's task.
  *
