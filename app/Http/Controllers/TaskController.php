f11bed7aa14b8782ce6dd115229567892eefd3f4
SKILL573 <a9101021249@gmail.com>
2017-10-13 15:14:42 +0800

    顯示已有的任務

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -20,5 +20,19 @@ public function store(Request $request)
 
     // Create The Task...
 }
-
+/**
+ * Display a list of all of the user's task.
+ *
+ * @param  Request  $request
+ * @return Response
+ */
+ public function index(Request $request)
+ {
+     $tasks = Task::where('user_id', $request->user()->id)->get();
+ 
+     return view('tasks.index', [
+         'tasks' => $tasks,
+     ]);
+ }
+ 
 }
