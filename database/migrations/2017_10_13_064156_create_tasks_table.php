153511cb38a767f75cfb057ec032afde4bf6e471
SKILL573 <a9101021249@gmail.com>
2017-10-13 14:43:36 +0800

    編輯TASK

--- database/migrations/2017_10_13_064156_create_tasks_table.php
+++ database/migrations/2017_10_13_064156_create_tasks_table.php
@@ -1,13 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 class CreateTasksTable extends Migration
 {
     /**
-     * Run the migrations.
+     * 執行遷移。
      *
      * @return void
      */
@@ -15,17 +14,19 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->index();
+            $table->string('name');
             $table->timestamps();
         });
     }
 
     /**
-     * Reverse the migrations.
+     * 還原遷移。
      *
      * @return void
      */
     public function down()
     {
-        Schema::dropIfExists('tasks');
+        Schema::drop('tasks');
     }
 }
