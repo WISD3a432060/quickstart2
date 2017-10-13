3826d79761e18cadcea2f3ce763f9a51e570cf91
SKILL573 <a9101021249@gmail.com>
2017-10-13 14:55:40 +0800

    任務控制器

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,6 @@ Route::auth();
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/tasks', 'TaskController@index');
+Route::post('/task', 'TaskController@store');
+Route::delete('/task/{task}', 'TaskController@destroy');
