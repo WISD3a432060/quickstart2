ffc9f33dcf51469870ba4775afaf36bb50580983
SKILL573 <a9101021249@gmail.com>
2017-11-24 14:02:12 +0800

    修改內容

--- app/Task.php
+++ app/Task.php
@@ -18,4 +18,26 @@ class Task extends Model
         echo $task->name;
     }
     
+    use App\User;
+    use Illuminate\Database\Eloquent\Model;
+    class Task extends Model
+    {
+        /**
+         * 這些屬性能被批量賦值。
+         *
+         * @var array
+         */
+        protected $fillable = ['name'];
+    
+        /**
+         * 取得擁有此任務的使用者。
+         */
+        public function user()
+        {
+            return $this->belongsTo(User::class);
+        }
+    }
+
+    
+    
 }
